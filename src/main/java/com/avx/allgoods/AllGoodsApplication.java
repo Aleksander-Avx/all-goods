b8ecc520f1ca918b11aed4c0d78dcde9820dce80
Khorev Alexander <xew1993@gmail.com>
2023-02-03 09:53:09 +0300

    начало разработки

---
+++ src/main/java/com/avx/allgoods/AllGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.avx.allgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllGoodsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avx/allgoods/AllGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avx.allgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
